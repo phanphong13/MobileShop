606cfa335d1af64bec3da98f12ebb28a57227741
tranmanh2672002 <tranmanh2672002>
2022-05-16 18:10:36 +0700

    update Category

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -19,29 +19,43 @@ var btnSideBar__type = document.querySelector(".gird__sidebar-type");
 if (btnSideBar__price) {
     var list_price = btnSideBar__price.querySelector(".gird__sidebar-list");
     var btn_price =  btnSideBar__price.querySelector(".gird__sidebar-btn");
-    btnSideBar__price.onclick = function() {
+    btnSideBar__price.addEventListener('click' , function() {
         list_price.classList.toggle('gird__sidebar-list-hide');
         btn_price.classList.toggle('gird__sidebar-btn-active');
+    })
 
-    }
+    list_price.addEventListener('click' , function(event) {
+        event.stopPropagation();
+        
+    })
 }
 if (btnSideBar__vendor) {
     var list_vendor = btnSideBar__vendor.querySelector(".gird__sidebar-list");
     var btn_vendor =  btnSideBar__vendor.querySelector(".gird__sidebar-btn");
 
-    btnSideBar__vendor.onclick = function() {
+    btnSideBar__vendor.addEventListener('click' , function() {
         list_vendor.classList.toggle('gird__sidebar-list-hide');
         btn_vendor.classList.toggle('gird__sidebar-btn-active');
-    }
+    })
+
+    list_vendor.addEventListener('click' , function(event) {
+        event.stopPropagation();
+        
+    })
 }
 if (btnSideBar__type) {
     var list_type = btnSideBar__type.querySelector(".gird__sidebar-list");
     var btn_type =  btnSideBar__type.querySelector(".gird__sidebar-btn");
 
-    btnSideBar__type.onclick = function() {
+    btnSideBar__type.addEventListener('click' , function() {
         list_type.classList.toggle('gird__sidebar-list-hide');
         btn_type.classList.toggle('gird__sidebar-btn-active');
-    }
+    })
+
+    list_type.addEventListener('click' , function(event) {
+        event.stopPropagation();
+        
+    })
 }
 
 // switch btn dang ki dang nhap
